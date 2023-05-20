805cacf40ce7038ed8c9ab762f82ac8ee4025d41
Anthony C <44069058+4nth0ny1@users.noreply.github.com>
2023-05-20 15:49:51 -0500

    Form validation (#4)
    * added form validations from rhf
    
    * added errors
    
    * added additional validations
    
    ---------
    
    Co-authored-by: Anthony Catullo <anthonycatullo@oatslam.cable.rcn.com>

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -12,7 +12,8 @@ type FormValues = {
 
 const YouTubeForm = () => {
   const form = useForm<FormValues>();
-  const { register, control, handleSubmit } = form;
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
 
   const onSubmit = (data: FormValues) => {
     console.log("Form Submitted", data);
@@ -22,15 +23,64 @@ const YouTubeForm = () => {
   return (
     <div>
       <h1>YouTube Form ({renderCount / 2})</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register("username")} />
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <div className="form-control">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            {...register("username", {
+              required: { value: true, message: "Username is required" },
+            })}
+          />
+          <p style={{ color: "red", textAlign: "left" }}>
+            {errors.username?.message}
+          </p>
+        </div>
 
-        <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" {...register("email")} />
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", {
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Invalid email format",
+              },
+              validate: {
+                notAdmin: (fieldValue) => {
+                  return (
+                    fieldValue !== "admin@example.com" ||
+                    "Enter a different email address"
+                  );
+                },
+                notBlackListed: (fieldValue) => {
+                  return (
+                    !fieldValue.endsWith("baddomain.com") ||
+                    "This domain is not supported"
+                  );
+                },
+              },
+            })}
+          />
+          <p style={{ color: "red", textAlign: "left" }}>
+            {errors.email?.message}
+          </p>
+        </div>
 
-        <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register("channel")} />
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            {...register("channel", { required: "Channel is required" })}
+          />
+          <p style={{ color: "red", textAlign: "left" }}>
+            {errors.channel?.message}
+          </p>
+        </div>
 
         <button>Submit</button>
       </form>
