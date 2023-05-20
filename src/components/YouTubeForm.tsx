c81f9d4ac89fc3814c0cb24669664945d71e6427
Anthony C <44069058+4nth0ny1@users.noreply.github.com>
2023-05-20 11:46:48 -0500

    added rhf library and devtools (#2)
    Co-authored-by: Anthony Catullo <anthonycatullo@oatslam.local>

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,20 +1,29 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
+let renderCount = 0;
 
 const YouTubeForm = () => {
+  const form = useForm();
+  const { register, control } = form;
+  renderCount++;
   return (
     <div>
+      <h1>YouTube Form ({renderCount / 2})</h1>
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <input type="text" id="username" {...register("username")} />
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" {...register("email")} />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input type="text" id="channel" {...register("channel")} />
 
         <button>Submit</button>
       </form>
+      <DevTool control={control} />
     </div>
   );
 };
