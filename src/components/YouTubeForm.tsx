98df10a98750a98bafc64261a096d2c88498879c
Anthony Catullo <anthonycatullo@oatslam.cable.rcn.com>
2023-05-20 15:31:24 -0500

    added errors

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -12,7 +12,8 @@ type FormValues = {
 
 const YouTubeForm = () => {
   const form = useForm<FormValues>();
-  const { register, control, handleSubmit } = form;
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
 
   const onSubmit = (data: FormValues) => {
     console.log("Form Submitted", data);
@@ -23,34 +24,49 @@ const YouTubeForm = () => {
     <div>
       <h1>YouTube Form ({renderCount / 2})</h1>
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          {...register("username", {
-            required: { value: true, message: "Username is required" },
-          })}
-        />
+        <div className="form-control">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            {...register("username", {
+              required: { value: true, message: "Username is required" },
+            })}
+          />
+          <p style={{ color: "red", textAlign: "left" }}>
+            {errors.username?.message}
+          </p>
+        </div>
 
-        <label htmlFor="email">E-mail</label>
-        <input
-          type="email"
-          id="email"
-          {...register("email", {
-            pattern: {
-              value:
-                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-              message: "Invalid email format",
-            },
-          })}
-        />
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", {
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Invalid email format",
+              },
+            })}
+          />
+          <p style={{ color: "red", textAlign: "left" }}>
+            {errors.email?.message}
+          </p>
+        </div>
 
-        <label htmlFor="channel">Channel</label>
-        <input
-          type="text"
-          id="channel"
-          {...register("channel", { required: "Channel is required" })}
-        />
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            {...register("channel", { required: "Channel is required" })}
+          />
+          <p style={{ color: "red", textAlign: "left" }}>
+            {errors.channel?.message}
+          </p>
+        </div>
 
         <button>Submit</button>
       </form>
